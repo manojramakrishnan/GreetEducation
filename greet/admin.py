af3b5cc140bc2c3bd43ee8ab8b0e818f212d01e7
akhil murali <akhilmurali662@gmail.com>
2023-07-07 17:28:33 +0530

    added teacherextraadmin

--- greet/admin.py
+++ greet/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import StudentExtra
+from .models import TeacherExtra
 
 
 class StudentExtraAdmin(admin.ModelAdmin):
@@ -7,3 +8,6 @@ class StudentExtraAdmin(admin.ModelAdmin):
 admin.site.register(StudentExtra,StudentExtraAdmin)
 
 # Register your models here.
+class TeacherExtraAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(TeacherExtra,TeacherExtraAdmin)
